2e21141fbed4232ec6c6142e5fba37927f11c5ef
XanderUZZZER <xanderuzzzer@gmail.com>
2019-03-05 23:45:11 +0200

    page 700

---
+++ UsingViewComponents/Models/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsingViewComponents.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+        void AddProduct(Product newProduct);
+    }
+}
