8211c5b3ebff063b9aa5a90c9b86432ee161c94b
XanderUZZZER <xanderuzzzer@gmail.com>
2019-03-06 11:51:59 +0200

    chapter 22 done

---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke(bool showList)
+        {
+            if (showList)
+            {
+                return View("CityList", repository.Cities);
+            }
+            else
+            {
+                return View(new CityViewModel
+                {
+                    Cities = repository.Cities.Count(),
+                    Population = repository.Cities.Sum(c => c.Population)
+                });
+            }
+        }
+
+        //public IViewComponentResult Invoke()
+        //{
+        //    return View(new CityViewModel
+        //    {
+        //        Cities = repository.Cities.Count(),
+        //        Population = repository.Cities.Sum(c => c.Population)
+        //    });
+        //}
+
+        //public string Invoke()
+        //{
+        //    return $"{repository.Cities.Count()} cities, "
+        //    + $"{repository.Cities.Sum(c => c.Population)} people";
+        //}
+    }
+}
